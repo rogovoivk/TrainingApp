b411169b52cfaf29b276622c27c474cc7d5923c7
rogovoivk <33112170+rogovoivk@users.noreply.github.com>
2022-04-18 23:36:09 +1000

    Initial commit

---
+++ fefuactivity/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  fefuactivity
+//
+//  Created by Andrew L on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
